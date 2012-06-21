a600ed5ada79a5e687193531b82e4115c0c14bf7
multunus <mu0012@mu0012-desktop.(none)>
2012-06-21 15:44:48 +0530

    added new contact form

--- contact-main.js
+++ contact-main.js
@@ -19,7 +19,7 @@
 
 	ContactTypeView = Backbone.View.extend({
 		
-		el: $(".contact-type-list"),
+		el: $(".contact-type-view"),
 		template: _.template($("#contact-type-template").html()),
 
 		initialize: function(){
@@ -32,7 +32,7 @@
 			//console.log(this.collection.getByCid("c1"));
 			_.each(this.getTypes(),function(item){
 				//$(".contact-type-list").append(this.template({"type": item}))
-				this.$el.append(this.template({"type": item}))
+				this.$el.find(".contact-type-list").append(this.template({"type": item}))
 				//console.log(item);
 				//console.log($(".contact-type-view"));
 			},this);
@@ -42,7 +42,15 @@
 		getTypes: function(){
 			//console.log(_.uniq(this.collection.pluck("type")));
 			return  _.uniq(this.collection.pluck("type"));
+		},
+			events: {
+			"click #new-contact-button" : "renderForm"
+		},
+		renderForm: function(){
+			//contactsRouter.navigate("new");
+			contactView.trigger("click:newContact");
 		}
+
 
 
 	});
@@ -58,29 +66,95 @@
 
 	});
 
+	ContactFormView = Backbone.View.extend({
+		//el: $(".contact-main-area"),
+		className: "contact-form-container",
+		template: _.template($("#contact-form-template").html()),
+		initialize: function(){
+			
+		},
+		render: function(){
+			this.$el.html(this.template());
+			return this;
+
+		}
+	})
+
 	ContactsMainView = Backbone.View.extend({
-		el: $(".contact-list"),
+		el: $(".contact-main-area"),
 
 		initialize: function(){
 			this.collection = new ContactCollection(contacts);
 			this.render();
+			this.collection.on("reset",this.render,this);
+			this.on("click:filterType",this.filterByType,this);
+			this.on("click:newContact",this.renderForm,this);
+
 
 		},
 		render: function(){
+
+			this.$el.find("tr:has(td)").remove();
 			_.each(this.collection.models,function(item){
 				var cv = new ContactView({model: item});
-				this.$el.append(cv.render().el);
+				if(this.$el.find(".contact-list").length == 0){
+					//this.$el.append()
+				}
+				this.$el.find(".contact-list").append(cv.render().el);
 			},this)
+		},
+		renderForm: function(){
+			this.$el.find(".contact-list").remove();
+			var contactForm = new ContactFormView();
+			this.$el.append(contactForm.render().el);
+			
+
+			//console.log(contactForm.render().el);
+
+		},
+		filterByType: function(){
+			console.log("navigated to "+this.filterType);
+			if(this.filterType == "all"){
+				this.collection.reset(contacts);	
+			}
+			else{
+				console.log("filterType is "+this.filterType);
+				this.collection.reset(contacts, { silent: true });
+				var filteredContacts = _.filter(this.collection.models,function(item){
+						 return item.get("type") == this.filterType;
+						// console.log(this.filterType);
+						},this);
+
+				this.collection.reset(filteredContacts);
 		}
+	}
+	
 
+	});
 
+	ContactsRouter = Backbone.Router.extend({
+		routes: {
+			"type/:type" : "filterContact"
+			
+		},
+		filterContact: function(type){
+			contactView.filterType = type;
+			contactView.trigger("click:filterType");
+		}
+		
+			//console.log(contacts);
+			//contactView.collection.reset(contacts);
+		
 
-
-	})
+	});
 
 
 
 	var ctv = new ContactTypeView();
-	var cmv = new ContactsMainView();
+	var contactView = new ContactsMainView();
+	var contactsRouter = new ContactsRouter();
+	var cf = new ContactFormView();
+	//cf.render();
+	Backbone.history.start();
 		
 }(jQuery));
